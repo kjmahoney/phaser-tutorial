5b2c15244414ace8fcb9553dc25f6d849075a140
Kevin Mahoney <kjmahoney13@gmail.com>
2017-06-02 14:48:34 -0400

    add movement to player

---
+++ squareGame/js/start.js
@@ -0,0 +1,40 @@
+var game = new Phaser.Game(500,500, Phaser.AUTO);
+
+var box = function(options) {
+  var bmd = game.add.bitmapData(options.length, options.width);
+  bmd.ctx.beginPath();
+  bmd.ctx.rect(0,0,options.length,options.width);
+  bmd.ctx.fillStyle = options.color;
+  bmd.ctx.fill();
+  return bmd;
+}
+
+var mainState = {
+  create: function(){
+    game.stage.backgroundColor = '#BDC2C5';
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.world.enableBody = true;
+
+    this.player = game.add.sprite(32,32,box({length:32, width: 32, color: '#FF0000'}))
+    this.cursor = game.input.keyboard.createCursorKeys();
+  },
+  update: function(){
+    var speed = 250;
+    this.player.body.velocity.y = 0;
+    this.player.body.velocity.x = 0;
+
+    if (this.cursor.up.isDown) {
+      this.player.body.velocity.y -= speed;
+    } else if (this.cursor.down.isDown) {
+      this.player.body.velocity.y += speed;
+    }
+    if (this.cursor.left.isDown) {
+      this.player.body.velocity.x -= speed;
+    } else if (this.cursor.right.isDown) {
+      this.player.body.velocity.x += speed;
+    }
+  }
+}
+
+game.state.add('main',mainState);
+game.state.start('main');
