e8ed0e9f6a4474365028ae2863393f83054e53f9
Kevin Mahoney <kjmahoney13@gmail.com>
2017-06-07 23:26:49 -0400

    move walls js

--- squareGame/js/start.js
+++ squareGame/js/start.js
@@ -24,56 +24,7 @@ var mainState = {
     this.cursor = game.input.keyboard.createCursorKeys();
     this.player.body.collideWorldBounds = true;
 
-    this.walls = game.add.group();
-    this.walls.enableBody = true;
-
-    var top = this.walls.create(0,0,
-      box({
-        length: game.world.width,
-        width: 16,
-        color: '#374A59'
-      }));
-    top.body.immovable = true;
-
-    var bottom = this.walls.create(0,game.world.height - 16,
-      box({
-        length: game.world.width,
-        width: 16,
-        color: '#374A59'
-      }));
-    bottom.body.immovable = true;
-
-    var leftWall = this.walls.create(0,16,
-      box({
-        length: 16,
-        width: game.world.height - 32,
-        color: '#374A59'
-      }));
-    leftWall.body.immovable = true;
-
-    var rightWall = this.walls.create(game.world.width - 16, 16,
-      box({
-        length: 16,
-        width: game.world.height - 32,
-        color: '#374A59'
-      }));
-    rightWall.body.immovable = true;
-
-    var innerWall1 = this.walls.create(game.world.width /4, 16,
-      box({
-        length: 16,
-        width: game.world.height - game.world.height/4,
-        color: '#374A59'
-      }));
-    innerWall1.body.immovable = true;
-
-    var innerWall2 = this.walls.create(game.world.width /2, 128,
-      box({
-        length: 16,
-        width: game.world.height - game.world.height/4,
-        color: '#374A59'
-      }));
-    innerWall2.body.immovable = true;
+    this.walls = walls(game);
 
     this.enemy = game.add.sprite(200, 32,
       box({
@@ -83,6 +34,7 @@ var mainState = {
       }))
   },
   update: function(){
+
     game.physics.arcade.collide(this.player, this.walls);
 
     var speed = 250;
--- squareGame/js/walls.js
+++ squareGame/js/walls.js
@@ -1,1 +1,54 @@
-console.log('walls working!')
+function walls(game) {
+  this.walls = game.add.group();
+  this.walls.enableBody = true;
+
+  var top = this.walls.create(0,0,
+    box({
+      length: game.world.width,
+      width: 16,
+      color: '#374A59'
+    }));
+    top.body.immovable = true;
+
+  var bottom = this.walls.create(0,game.world.height - 16,
+    box({
+      length: game.world.width,
+      width: 16,
+      color: '#374A59'
+    }));
+    bottom.body.immovable = true;
+
+  var leftWall = this.walls.create(0,16,
+    box({
+      length: 16,
+      width: game.world.height - 32,
+      color: '#374A59'
+    }));
+    leftWall.body.immovable = true;
+
+    var rightWall = this.walls.create(game.world.width - 16, 16,
+      box({
+        length: 16,
+        width: game.world.height - 32,
+        color: '#374A59'
+      }));
+      rightWall.body.immovable = true;
+
+    var innerWall1 = this.walls.create(game.world.width /4, 16,
+      box({
+        length: 16,
+        width: game.world.height - game.world.height/4,
+        color: '#374A59'
+      }));
+      innerWall1.body.immovable = true;
+
+      var innerWall2 = this.walls.create(game.world.width /2, 128,
+        box({
+          length: 16,
+          width: game.world.height - game.world.height/4,
+          color: '#374A59'
+      }));
+      innerWall2.body.immovable = true;
+
+  return(this.walls);
+}
